7e407b60990e6da06272f68d279fb9780f47a1d1
Gustavo <gnferreira2000@gmail.com>
2021-03-25 15:31:32 -0300

    create: enterprise entity

---
+++ src/modules/enterprises/entities/Enterprise.ts
@@ -0,0 +1,61 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+import { User } from '../../users/entities/User';
+
+@Entity('enterprises')
+export class Enterprise {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  telephone: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  number: number;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @Column()
+  postalCode: string;
+
+  @Column()
+  user_id: string;
+
+  @OneToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
