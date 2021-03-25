876318b0a936194a0ad63ad339e7db10c7306d24
Gustavo <gnferreira2000@gmail.com>
2021-03-25 01:50:56 -0300

    create: applicant typeorm repository

---
+++ src/modules/applicants/repositories/implementations/ApplicationsRepository.ts
@@ -0,0 +1,35 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { ICreateApplicantDTO } from '../../dtos/ICreateApplicantDTO';
+import { Applicant } from '../../entities/Applicant';
+import { IApplicantsRepository } from '../IApplicantsRepository';
+
+export class ApplicationsRepository implements IApplicantsRepository {
+  private repository: Repository<Applicant>;
+
+  constructor() {
+    this.repository = getRepository(Applicant);
+  }
+
+  async create({
+    name,
+    biography,
+    telephone,
+    dateOfBirth,
+    schooling,
+    isWorking,
+    user_id,
+  }: ICreateApplicantDTO): Promise<void> {
+    const applicant = this.repository.create({
+      name,
+      biography,
+      telephone,
+      dateOfBirth,
+      schooling,
+      isWorking,
+      user_id,
+    });
+
+    await this.repository.save(applicant);
+  }
+}
