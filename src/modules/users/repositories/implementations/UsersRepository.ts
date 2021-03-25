da34fd06af8f1247a1402fb2bf7734ffca31a20a
Gustavo <gnferreira2000@gmail.com>
2021-03-25 01:47:52 -0300

    create: user typeorm repository

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,27 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create({
+    email,
+    password,
+    isEnterprise,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      email,
+      password,
+      isEnterprise,
+    });
+
+    await this.repository.save(user);
+  }
+}
